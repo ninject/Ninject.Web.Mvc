7f3f8e1b29b778eed6af5f354745b9dfc18c45ae
Nate Kohari <nate@enkari.com>
2009-03-15 22:00:58 -0400

    Initial commit, split from core

---
+++ src/Ninject.Web.Mvc/NinjectControllerFactory.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ninject.Web.Mvc
+{
+	/// <summary>
+	/// A controller factory that creates <see cref="IController"/>s via Ninject.
+	/// </summary>
+	public class NinjectControllerFactory : IControllerFactory
+	{
+		/// <summary>
+		/// Gets the kernel that will be used to create controllers.
+		/// </summary>
+		public IKernel Kernel { get; private set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NinjectControllerFactory"/> class.
+		/// </summary>
+		/// <param name="kernel">The kernel that should be used to create controllers.</param>
+		public NinjectControllerFactory(IKernel kernel)
+		{
+			Kernel = kernel;
+		}
+
+		/// <summary>
+		/// Creates the controller with the specified name.
+		/// </summary>
+		/// <param name="requestContext">The request context.</param>
+		/// <param name="controllerName">Name of the controller.</param>
+		/// <returns>The created controller.</returns>
+		public IController CreateController(RequestContext requestContext, string controllerName)
+		{
+			return Kernel.TryGet<IController>(controllerName);
+		}
+
+		/// <summary>
+		/// Releases the specified controller.
+		/// </summary>
+		/// <param name="controller">The controller to release.</param>
+		public void ReleaseController(IController controller) { }
+	}
+}
---
+++ src/Ninject.Web.Mvc/NinjectHttpApplication.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Ninject.Infrastructure;
+
+namespace Ninject.Web.Mvc
+{
+	/// <summary>
+	/// Defines an <see cref="HttpApplication"/> that is controlled by a Ninject <see cref="IKernel"/>.
+	/// </summary>
+	public abstract class NinjectHttpApplication : HttpApplication, IHaveKernel
+	{
+		private static IKernel _kernel;
+
+		/// <summary>
+		/// Gets the kernel.
+		/// </summary>
+		public IKernel Kernel
+		{
+			get { return _kernel; }
+		}
+
+		/// <summary>
+		/// Starts the application.
+		/// </summary>
+		public void Application_Start()
+		{
+			lock (this)
+			{
+				_kernel = CreateKernel();
+				ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(_kernel));
+				OnApplicationStarted();
+			}
+		}
+
+		/// <summary>
+		/// Stops the application.
+		/// </summary>
+		public void Application_Stop()
+		{
+			lock (this)
+			{
+				if (_kernel != null)
+				{
+					_kernel.Dispose();
+					_kernel = null;
+				}
+
+				OnApplicationStopped();
+			}
+		}
+
+		/// <summary>
+		/// Registers all controllers in the assembly with the specified name.
+		/// </summary>
+		/// <param name="assemblyName">Name of the assembly to search for controllers.</param>
+		public void RegisterAllControllersIn(string assemblyName)
+		{
+			RegisterAllControllersIn(Assembly.Load(assemblyName), GetControllerName);
+		}
+
+		/// <summary>
+		/// Registers all controllers in the assembly with the specified name.
+		/// </summary>
+		/// <param name="assemblyName">Name of the assembly to search for controllers.</param>
+		/// <param name="namingConvention">The naming convention to use for the controllers.</param>
+		public void RegisterAllControllersIn(string assemblyName, Func<Type, string> namingConvention)
+		{
+			RegisterAllControllersIn(Assembly.Load(assemblyName), namingConvention);
+		}
+
+		/// <summary>
+		/// Registers all controllers in the specified assembly.
+		/// </summary>
+		/// <param name="assembly">The assembly to search for controllers.</param>
+		public void RegisterAllControllersIn(Assembly assembly)
+		{
+			RegisterAllControllersIn(assembly, GetControllerName);
+		}
+
+		/// <summary>
+		/// Registers all controllers in the specified assembly.
+		/// </summary>
+		/// <param name="assembly">The assembly to search for controllers.</param>
+		/// <param name="namingConvention">The naming convention to use for the controllers.</param>
+		public void RegisterAllControllersIn(Assembly assembly, Func<Type, string> namingConvention)
+		{
+			foreach (Type type in assembly.GetExportedTypes().Where(IsController))
+				_kernel.Bind<IController>().To(type).Named(namingConvention(type));
+		}
+
+		private static bool IsController(Type type)
+		{
+			return typeof(IController).IsAssignableFrom(type) && type.IsPublic && !type.IsAbstract && !type.IsInterface;
+		}
+
+		private static string GetControllerName(Type type)
+		{
+			string name = type.Name.ToLowerInvariant();
+
+			if (name.EndsWith("controller"))
+				name = name.Substring(0, name.IndexOf("controller"));
+
+			return name;
+		}
+
+		/// <summary>
+		/// Creates the kernel that will manage your application.
+		/// </summary>
+		/// <returns>The created kernel.</returns>
+		protected abstract IKernel CreateKernel();
+
+		/// <summary>
+		/// Called when the application is started.
+		/// </summary>
+		protected virtual void OnApplicationStarted() { }
+
+		/// <summary>
+		/// Called when the application is stopped.
+		/// </summary>
+		protected virtual void OnApplicationStopped() { }
+	}
+}
