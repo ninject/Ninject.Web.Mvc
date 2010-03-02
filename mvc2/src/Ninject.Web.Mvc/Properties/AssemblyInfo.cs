85d5ba0b99197e8e35b1382e25bbc18315335410
Nate Kohari <nate@enkari.com>
2010-03-02 13:04:25 -0500

    Split between MVC and MVC2 projects, implemented changes to support MVC2 RC2

---
+++ mvc2/src/Ninject.Web.Mvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿#region License
+// Author: Nate Kohari <nate@enkari.com>
+// Copyright (c) 2007-2009, Enkari, Ltd.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+#region Using Directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+#endregion
+
+[assembly: AssemblyProduct("Ninject: Lightweight dependency injection for .NET")]
+[assembly: AssemblyTitle("Ninject ASP.NET MVC Integration")]
+[assembly: AssemblyCompany("Enkari, Ltd.")]
+[assembly: AssemblyCopyright("Copyright © 2007-2009 Enkari, Ltd. and contributors")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
