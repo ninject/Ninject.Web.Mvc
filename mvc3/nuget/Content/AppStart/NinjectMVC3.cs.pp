cfb2000a0df8d7163341e75f930f5f15b71c9bb4
Remo Gloor <remo.gloor@bbv.ch>
2011-03-01 18:43:32 +0100

    Changed to WebActivator 1.4 and implemented the new naming convention.

---
+++ mvc3/nuget/Content/AppStart/NinjectMVC3.cs.pp
@@ -0,0 +1,51 @@
+﻿[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.AppStart.NinjectMVC3), "Start")]
+[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof($rootnamespace$.AppStart.NinjectMVC3), "Stop")]
+
+namespace $rootnamespace$.AppStart
+{
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+	using Ninject.Web.Mvc;
+
+    public static class NinjectMVC3 
+	{
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+		{
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestModule));
+			bootstrapper.Initialize(CreateKernel);
+        }
+		
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+		public static void Stop()
+		{
+			bootstrapper.ShutDown();
+		}
+		
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+        }		
+    }
+}
