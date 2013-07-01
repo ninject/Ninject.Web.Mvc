1428d22cbdc66c73dad10c844841e773bc6846c3
Remo Gloor <remo.gloor@bbv.ch>
2013-07-01 17:49:22 +0200

    Added missing file for sample project

---
+++ mvc3/src/SampleApplication/Controllers/FilterInjectionExamples/LogAttribute.cs
@@ -0,0 +1,34 @@
+﻿//-------------------------------------------------------------------------------
+// <copyright file="LogAttribute.cs" company="bbv Software Services AG">
+//   Copyright (c) 2010 bbv Software Services AG
+//   Author: Remo Gloor (remo.gloor@gmail.com)
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+namespace SampleApplication.Controllers.FilterInjectionExamples
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class LogAttribute : Attribute
+    {
+        public string Prefix { get; private set; }
+
+        public LogAttribute(string prefix)
+        {
+            Prefix = prefix;
+        }
+    }
+}
